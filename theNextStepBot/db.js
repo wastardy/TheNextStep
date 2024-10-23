9e868ac472deb064373d68490782d569ec5f7873
Andrew <wastardy.k@gmail.com>
2024-10-23 17:05:28 +0300

    Initial Bot V1
    Searches for cafes by location and range in real time, saves in MongoDB

---
+++ theNextStepBot/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/the_next_step');
+        console.log('Successfully connected to the_next_step database');
+    }
+    catch (error) {
+        console.error('Error connecting to MongoDB', error.message);
+    }
+}
+
+module.exports = connectDB;
