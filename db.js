73397cea734779b23c37f545c8fbea36a03baa39
Andrew Kononenko <wastardy.k@gmail.com>
2024-11-14 19:34:48 +0200

    add connection string to hosted db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/the_next_step');
+        await mongoose.connect('mongodb+srv://developer_andrew:Variable2311@thenextstep.t6qek.mongodb.net/the_next_step');
         console.log('--------> Successfully connected to the_next_step database');
     }
     catch (error) {
